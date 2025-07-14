a73091c07fce27a08f6ff66372386c22ad5c97df
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-14 12:37:42 +0200

    refactor

--- src/Clock.hs
+++ src/Clock.hs
@@ -7,9 +7,9 @@ fromHourMin :: Int -> Int -> Clock
 fromHourMin h m = Clock (mod (h + div m 60) 24) (mod m 60)
 
 toString :: Clock -> String
-toString c = ad0 (hh c) ++ ":" ++ ad0 (mm c)
+toString c = pad (hh c) ++ ":" ++ pad (mm c)
   where
-    ad0 n = if n < 10 then "0" ++ show n else show n
+    pad n = show (div n 10) ++ show (mod n 10)
 
 addDelta :: Int -> Int -> Clock -> Clock
 addDelta dh dm c = fromHourMin (hh c + dh) (mm c + dm)
