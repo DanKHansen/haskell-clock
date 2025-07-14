9c8dda042c298e4add2418293ab55e84e83c1601
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-14 12:26:46 +0200

    solution

--- src/Clock.hs
+++ src/Clock.hs
@@ -1,13 +1,15 @@
 module Clock (addDelta, fromHourMin, toString) where
 
-data Clock = Dummy
-  deriving Eq
+data Clock = Clock {hh :: Int, mm :: Int}
+  deriving (Eq)
 
 fromHourMin :: Int -> Int -> Clock
-fromHourMin hour min = error "You need to implement this function."
+fromHourMin h m = Clock (mod (h + div m 60) 24) (mod m 60)
 
 toString :: Clock -> String
-toString clock = error "You need to implement this function."
+toString c = ad0 (hh c) ++ ":" ++ ad0 (mm c)
+  where
+    ad0 n = if n < 10 then "0" ++ show n else show n
 
 addDelta :: Int -> Int -> Clock -> Clock
-addDelta hour min clock = error "You need to implement this function."
+addDelta dh dm c = fromHourMin (hh c + dh) (mm c + dm)
